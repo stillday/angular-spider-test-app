6deb67f2dd1ed9333bc624a6bccde3f2e44494d9
Jan Drenkhahn <jan-dennis.drenkhahn@kraftwerk.co.at>
2019-08-06 17:51:43 +0200

    include first dropdown

---
+++ src/app/app.dropdown.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms";
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppDropdown {
+  
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ChartsModule } from 'ng2-charts';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AppDropdown } from './app.dropdown';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ChartsModule],
-  declarations: [AppComponent, HelloComponent],
+  imports: [BrowserModule, FormsModule, ChartsModule, ReactiveFormsModule],
+  declarations: [AppComponent, HelloComponent, AppDropdown],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
